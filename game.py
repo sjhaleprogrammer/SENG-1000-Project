21b5ac0a7ddfa53e3e66d15a8d5ab95dccfbcb35
Samuel <samworlds1231337@gmail.com>
2022-11-07 09:06:06 -0500

    abstracted functions into own class

---
+++ game.py
@@ -0,0 +1,54 @@
+
+
+class Game:
+
+
+    def __init__(self):
+        self.list = []
+    
+
+    def add(self):
+        x = input()
+        self.list.append(x)
+        print(f"{x} has been added to the list")
+        
+
+    def remove(self):
+        x = input()
+        if x in list:
+            self.list.remove(x)
+        else:
+            print(f"{x} is not in the list")
+
+
+    def displaymenu(self):
+
+        while True:
+            print('------------------------------------------------')
+            print('Input 0 to exit')
+            print('Input 1 to add a variable to the list')
+            print('Input 2 to remove a variable from the list ')
+            print('Input 3 to display the list')
+            print('------------------------------------------------')
+            
+
+            option = int(input())
+        
+            if option == 1:
+                self.add()
+                print(self.list)
+                print('')
+            elif option == 2:
+                self.remove()
+                print(self.list)
+                print('')
+            elif option == 3:
+                print(self.list)
+                print('')
+            elif option == 0:
+                print(self.list)
+                exit()
+
+
+    
+    
--- project.py
+++ project.py
@@ -1,28 +1,4 @@
-from menu import Menu
-
-
-list = []
-
-def add():
-    x = input()
-    list.append(x)
-    print(f"{x} has been added to the list")
-    
-
-def remove():
-    x = input()
-    if x in list:
-        list.remove(x)
-    else:
-        print(f"{x} is not in the list")
-
-
-#def pickup
-
-#def comsume 
-
-
-
+from game import Game
 
 
 
@@ -30,22 +6,7 @@ def remove():
 
 if __name__ == "__main__":
   
-    while True:
-        Menu().display()
-
-        option = int(input())
     
-        if option == 1:
-            add()
-            print(list)
-            print('')
-        elif option == 2:
-            remove()
-            print(list)
-            print('')
-        elif option == 3:
-            print(list)
-            print('')
-        elif option == 0:
-            print(list)
-            exit()
+    Game().displaymenu()
+
+        
