0c081c22ed6f6128cddc56eae6f3a3bd1838ad4c
OffshoreFlyer96 <ryanpatacsil@gmail.com>
2022-11-11 14:59:18 -0500

    Added rough evasion

--- testing.py
+++ testing.py
@@ -5,11 +5,16 @@ from enemy import Enemy
 testDamage = random.randint(0,100)
 testHealth = 100
 testResistance = random.randint(0,100)
+testEvade = random.randint(0,100)
+testEvadeCheck = random.randint(0,100)
 if testResistance > 80:
     testResistance = 80
 def fightTest():
-    print(testResistance, testDamage, testHealth)
+    print(testResistance, testDamage, testHealth, testEvade, testEvadeCheck)
     factored_damage = testDamage * ((100-testResistance)/100)
-    health_difference = testHealth - factored_damage
+    if testEvade > testEvadeCheck:
+        health_difference = testHealth - factored_damage
+    else:
+        health_difference = testHealth
     print(health_difference)
 fightTest()
