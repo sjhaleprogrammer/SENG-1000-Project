fab9a7e64d63015fff3a926b704dbf164b6944e3
EricLarwa <larwae22@students.ecu.edu>
2022-12-02 09:44:02 -0500

    Added Items (Damage and Health potions)

--- item.py
+++ item.py
@@ -1,10 +1,29 @@
+import random
+import time
 
+from enemy import Enemy
+from player import Player
+class Item:
 
-class item:
+    def __init__(self):
+        self.enemy = Enemy()
+        self.player = Player()
 
-    def __init__(self,name,damage):
-        self.name = ""
-        self.damage = 0
+    def damage_potion(self,enemy):
 
+        enemy.health -= random.randint(5,10)
 
-    
+        print(f'{self.enemy} now has {enemy.health} health!')
+
+    def health_potion(self, player):
+
+        player.health += random.randint(5,10)
+
+        print(f'{self.player} now has {player.health} health!')
+
+    def greater_health_potion(self, player):
+
+        player.health += random.randint(10,20)
+
+        print(f'{self.player} now has {player.health} health!')
+
